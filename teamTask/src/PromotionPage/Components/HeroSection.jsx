55c1e894260273cf14f4e9c53294c13fc4330f04
Akash Kushwaha <23kushwahaakash@gmail.com>
2025-11-16 19:37:43 +0530

    Made login and signup button functional

--- teamTask/src/PromotionPage/Components/HeroSection.jsx
+++ teamTask/src/PromotionPage/Components/HeroSection.jsx
@@ -1,6 +1,8 @@
 import HeroImage from "../Images/HeroImage.png"
+import { useNavigate } from "react-router-dom"
 
 function HeroSection() {
+    const navigate=useNavigate()
   return (
     <div className='relative md:min-h-screen pt-10 md:pt-6 z-0'>
         <img src={HeroImage} alt="map" className="relative md:min-h-screen "/>
@@ -10,8 +12,12 @@ function HeroSection() {
                 <h1 className="text-white text-xl md:text-6xl text-center font-serif mb-3">"Find Your Next Opportunity.Hire Your Perfect Candidate"</h1>
                 <h1 className="text-white text-center text-xs md:text-2xl">"The all-in-one platform that connects top talent with leading companies."</h1>
                 <div className="flex mt-2 md:mt-30 gap-5">
-                   <button className="text-white bg-[#1c58c1] md:text-4xl px-5 md:px-10 md:py-2 py-1 rounded-md">Log In</button>
-                   <button className="text-white bg-[#1c58c1] md:text-4xl px-5 md:px-10 md:py-2 py-1 rounded-md">Sign Up</button>
+                   <button 
+                   className="text-white bg-[#1c58c1] md:text-4xl px-5 md:px-10 md:py-2 py-1 rounded-md cursor-pointer"
+                   onClick={()=>navigate("/login")}>Log In</button>
+                   <button 
+                   className="text-white bg-[#1c58c1] md:text-4xl px-5 md:px-10 md:py-2 py-1 rounded-md cursor-pointer"
+                   onClick={()=>navigate("/signup")}>Sign Up</button>
                 </div>
             </div>
         </div>
